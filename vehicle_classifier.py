6b7abfc68505ac7fdda101ed3fa55f31835eecef
BuildTools <theguruofreason@gmail.com>
2017-06-24 22:23:10 -0700

    now uses HSV for HOG, test accuracy around 99%

--- vehicle_classifier.py
+++ vehicle_classifier.py
@@ -31,22 +31,14 @@ def convert_color(img, conv='RGB2YCrCb'):
 
 def get_hog_features(img, orient, pix_per_cell, cell_per_block,
                      vis=False, feature_vec=True):
-    # Call with two outputs if vis==True
-    if vis == True:
-        features, hog_image = hog(img, orientations=orient,
-                                  pixels_per_cell=(pix_per_cell, pix_per_cell),
-                                  cells_per_block=(cell_per_block, cell_per_block),
-                                  transform_sqrt=False,
-                                  visualise=vis, feature_vector=feature_vec)
-        return features, hog_image
-    # Otherwise call with one output
-    else:
-        features = hog(img, orientations=orient,
-                       pixels_per_cell=(pix_per_cell, pix_per_cell),
-                       cells_per_block=(cell_per_block, cell_per_block),
-                       transform_sqrt=False,
-                       visualise=vis, feature_vector=feature_vec)
-        return features
+    features = []
+    for channel in range(img.shape[2]):
+        features.append(hog(img[:,:,channel], orientations=orient,
+                              pixels_per_cell=(pix_per_cell, pix_per_cell),
+                              cells_per_block=(cell_per_block, cell_per_block),
+                              transform_sqrt=False,
+                              visualise=vis, feature_vector=feature_vec))
+    return np.concatenate(features)
 
 
 # returns a rescaled list of lists of color channel values
@@ -130,7 +122,7 @@ non_car_examples = non_car_image_filenames
 spatial_colorspace = 'BGR'
 spatial_size = (32, 32)
 histogram_bins = 16
-hog_conversion = 'BGR2GRAY'
+hog_conversion = 'BGR2HSV'
 orient = 12
 pix_per_cell = 8
 cell_per_block = 2
